9cb38289f1f34716b0a40b421e0a9e9e1ce6f89c
AbdulWahid <abdulwahidbuttpk@gmail.com>
2020-05-23 14:46:47 +0500

    Create Wallet using Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from './component/shared/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './component/dashboard/Dashboard';
+import Welcome from './component/Welcome';
+import CreateWallet from './component/dashboard/dashboardoperations/CreateWallet';
+import NotFound from './component/shared/NotFound';
+
+import { Provider } from 'react-redux'
+import store from './Store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <Route path="/" component={Nav} />
+        <Switch>
+          <Route path="/" exact component={Welcome} />
+          <Route path="/dashboard" exact component={Dashboard} />
+          <Route path="/createwallet" exact component={CreateWallet} />
+          <Route path="/" component={NotFound} />
+
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+
   );
 }
 
